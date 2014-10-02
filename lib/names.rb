b70461277713705bc9af9fcee4b1ecfc21672563
danjocutler <danjocutler@gmail.com>
2014-10-02 19:42:24 +0100

    Added stylesheet

--- lib/names.rb
+++ lib/names.rb
@@ -1,12 +1,13 @@
 class Competitors
 
 	def names
-		%w(Alex Ella Nicole Dan Hadi Craig Colin Rachel Hastings Hercules Stephen Denise Sandrine Victoria Scully Fadie Elena Pablo Zeeshan Camilla Yvette James Karin Sean)
+		%w(Alex Ella Nicole Dan Hadi Craig Colin Rachel Hastings Bernard Stephen Denise Sandrine Victoria Scully Fadie Elena Pablo Zeeshan Camilla Yvette James Karin Sean)
 	end
 
 	def sort_groups
 		names.shuffle.each_slice(6).to_a
 	end
+
 end
 
 
